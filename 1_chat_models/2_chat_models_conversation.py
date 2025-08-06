eb5bcb603b29cfbedc667a52b5820a977d6f6e08
rakesh <rakesh.racharla1@gmail.com>
2025-08-06 13:56:12 +0100

    replaced chatopenai with google ai

--- 1_chat_models/2_chat_models_conversation.py
+++ 1_chat_models/2_chat_models_conversation.py
@@ -1,10 +1,13 @@
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o")
+# llm = ChatOpenAI(model="gpt-4o")
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
 messages = [
     SystemMessage("You are an expert in social media content strategy"), 
